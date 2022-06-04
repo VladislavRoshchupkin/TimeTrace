392fb83e8288a527b15d32b0a29863f73d9a76a4
Влад Рощупкин <roshchupkin.vladislav@mail.ru>
2022-06-04 23:00:25 +0300

    Так же убран коммит

--- app/utils.py
+++ app/utils.py
@@ -8,27 +8,24 @@ class Calendar(HTMLCalendar):
 		self.month = month
 		super(Calendar, self).__init__()
 
-	# formats a day as a td
-	# filter events by day
+	
 	def formatday(self, day, events):
 		events_per_day = events.filter(date_work__day=day)
 		d = ''
-		# for event in events_per_day:
-		# 	d += f'<li> {event.title} </li>'
+		
 
 		if day != 0:
 			return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
 		return '<td></td>'
 
-	# formats a week as a tr 
+	
 	def formatweek(self, theweek, events):
 		week = ''
 		for d, weekday in theweek:
 			week += self.formatday(d, events)
 		return f'<tr> {week} </tr>'
 
-	# formats a month as a table
-	# filter events by year and month
+	
 	def formatmonth(self, withyear=True):
 		events = Time.objects.filter(date_work__year=self.year, date_work__month=self.month)
 
